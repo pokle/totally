b8f860f734041280af1a0241d5b516924d8d6a47
Tushar Pokle <tushar.pokle@gmail.com>
2014-04-10 21:50:54 +1000

    Logs needs to be run without a pseudo-terminal because we need to terminate the while loop on ^C

--- src/commands/logs.sh
+++ src/commands/logs.sh
@@ -1,5 +1,5 @@
 function cmd_logs() {
-    on_docker_host "
+    on_docker_host --non-interactively "
         echo -- Tailing logs for $CONTAINER. Hit ^C to stop
         while true; do
             docker logs --follow $CONTAINER 
